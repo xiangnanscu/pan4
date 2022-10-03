89d88adde206ac957e318a07b64f07e5016a3e69
xn <280145668@qq.com>
2022-10-03 11:31:44 +0800

    x

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,53 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+  <Page>
+    <ActionBar>
+      <Label text="Home" />
+    </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+    <GridLayout>
+      <Label class="info">
+        <FormattedString>
+          <Span class="fas" text.decode="&#xf135; " />
+          <Span :text="message" />
+          <Button @click="add">add</Button>
+        </FormattedString>
+      </Label>
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
+export default {
+  data() {
+    return {
+      cnt:0
     }
-  };
+  },
+  methods: {
+    add() {
+      this.cnt++
+    }
+  },
+  computed: {
+    message() {
+      return `Blank {N}-Vue app? ${this.cnt}`;
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+@import '@nativescript/theme/scss/variables/blue';
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+// Custom styles
+.fas {
+  @include colorize($color: accent);
+}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+  font-size: 20;
+  horizontal-align: center;
+  vertical-align: center;
+}
 </style>
+
